b4bd34802cab1802f680093f9ae3bc42aa6a8fe4
yozora <128562373+luciousPow@users.noreply.github.com>
2024-01-26 19:45:31 +0800

    Add files via upload

---
+++ wrapper_classes.java
@@ -0,0 +1,19 @@
+package ForPract;
+
+public class wrapper_classes {
+
+	public static void main(String[] args) {
+		
+		Boolean a = true;
+		Character b = '@';
+		Integer c = 123;
+		Double d = 3.14;
+		String e = "Hello";
+		
+		if (a==true) {
+			System.out.println("This is true");
+		}
+
+	}
+
+}
