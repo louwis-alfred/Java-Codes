71a2b9e0ac104f6c09dbd5a4f55bc558b5d9fdff
yozora <128562373+luciousPow@users.noreply.github.com>
2024-01-26 19:46:00 +0800

    Add files via upload

---
+++ String_methods.java
@@ -0,0 +1,24 @@
+package ForPract;
+
+public class String_methods {
+
+	public static void main(String[] args) {
+		
+		
+		String name ="Coleen";
+		
+		//boolean result = name.equalsIgnoreCase("Coleen");
+		//int result = name.length();
+		//char result = name.charAt(0)
+		//int result = name.indexOf("C");
+		//boolean result = name.isEmpty();
+		//String result = name.toUpperCase();
+		//String result = name.toLowerCase();
+		//String result = name.trim();
+		String result = name.replace('e', 'i');
+		
+		System.out.println(result);
+
+	}
+
+}
