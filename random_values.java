891e2f0a2abfc97bdfa08231a7768a6c99269e25
yozora <128562373+luciousPow@users.noreply.github.com>
2024-01-26 19:47:03 +0800

    Add files via upload

---
+++ random_values.java
@@ -0,0 +1,24 @@
+package ForPract;
+import java.util.Random;
+public class random_values {
+
+	public static void main(String[] args) {
+		
+		Random r = new Random();
+		
+		//int x = r.nextInt(6)+1;
+		//double y = r.nextDouble();
+		boolean y = r.nextBoolean();
+		
+		
+		
+		System.out.println(y);
+		
+		
+		
+		
+			
+
+	}
+
+}
