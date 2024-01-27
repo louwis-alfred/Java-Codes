1cb924187af24f9d623a5124eb375882ae9354a6
yozora <128562373+luciousPow@users.noreply.github.com>
2024-01-27 18:45:55 +0800

    Add files via upload

---
+++ summation_of_numbers_return.java
@@ -0,0 +1,24 @@
+package ForPract;
+
+public class summation_of_numbers_return {
+
+	public static void main(String[] args) {
+		
+		int numbers[] = {1,2,3,4,5};
+		
+		int result = summation(numbers);
+		System.out.println(result);
+
+	}
+	
+	static int summation(int numbers[]) {
+		int sum = 0;
+		
+		for(int number: numbers) {
+			sum = sum + number;
+		}
+		
+		return sum;
+	}
+
+}
