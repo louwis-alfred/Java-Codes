8fef819e3d0b5c3e6a5d39506c4ce167181d4638
yozora <128562373+luciousPow@users.noreply.github.com>
2024-01-27 18:48:12 +0800

    Add files via upload

---
+++ pract_print.java
@@ -0,0 +1,15 @@
+package ForPract;
+
+public class pract_print {
+
+	public static void main(String[] args) {
+		
+		print("Hello World")
+
+	;}
+	
+	static void print(String word) {
+		System.out.println(word);
+	}
+
+}
